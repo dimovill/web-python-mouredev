fa03519fbb6f3a0abeaaf939dee38adde9998a09
Diego Molina Villa <diegomolinavilla@gmail.com>
2023-09-03 13:34:23 +0200

    web contador hecha

--- web_python_mouredev/web_python_mouredev.py
+++ web_python_mouredev/web_python_mouredev.py
@@ -1,45 +1,35 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-from rxconfig import config
-
 import reflex as rx
-
-docs_url = "https://reflex.dev/docs/getting-started/introduction"
-filename = f"{config.app_name}/{config.app_name}.py"
 
 
 class State(rx.State):
-    """The app state."""
+    count: int = 0
 
-    pass
+    def increment(self):
+        self.count += 1
+
+    def decrement(self):
+        self.count -= 1
 
 
-def index() -> rx.Component:
-    return rx.fragment(
-        rx.color_mode_button(rx.color_mode_icon(), float="right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", font_size="2em"),
-            rx.box("Get started by editing ", rx.code(filename, font_size="1em")),
-            rx.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": rx.color_mode_cond(
-                        light="rgb(107,99,246)",
-                        dark="rgb(179, 175, 255)",
-                    )
-                },
-            ),
-            spacing="1.5em",
-            font_size="2em",
-            padding_top="10%",
+def index():
+    return rx.vstack( 
+    rx.hstack(
+        rx.button(
+            "Decrement",
+            color_scheme="red",
+            border_radius="1em",
+            on_click=State.decrement,
+        ),
+        rx.heading("hola",State.count, font_size="2em"),
+        rx.button(
+            "Increment",
+            color_scheme="green",
+            border_radius="1em",
+            on_click=State.increment,
         ),
     )
+		)
 
-
-# Add state and page to the app.
 app = rx.App()
 app.add_page(index)
 app.compile()
